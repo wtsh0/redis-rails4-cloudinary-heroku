5db5d005ac15a34a2bf3a475a1a8294e90a2729c
mac <toshi.import@gmail.com>
2018-10-13 19:55:04 +0900

    BootStrap3用のフラッシュメッセージ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+
   devise_for :managers, :controllers => {
     sessions: 'managers/sessions',
     registrations: 'managers/registrations',
     passwords: 'managers/passwords'
-  } 
-  
+  }
+
   namespace :managers do
     resources :articles
   end
