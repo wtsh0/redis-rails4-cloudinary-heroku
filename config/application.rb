4fb3b1956861738d9608e832bb0b19638a390859
mac <toshi.import@gmail.com>
2018-10-16 08:31:43 +0900

    管理者ログインのレイアウト変更

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,10 @@ module RedisRails
     config.active_record.default_timezone = :local
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    # for localizing language(separated directory tree)
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
