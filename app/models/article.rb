33ef316f28d2c9e475f7db6c0b744036e1b72c74
mac <toshi.import@gmail.com>
2018-10-16 09:13:43 +0900

    画像アップロード用の設定2

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
-
+  # image_uploader(cloudinary)
+  attr_accessor :image
+  mount_uploader :image, ArticleimageUploader
   def check_published_at
     self.published == true ? self.published_at = Time.now : self.published_at = ""
   end
