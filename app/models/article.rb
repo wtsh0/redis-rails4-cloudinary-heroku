6dc5bfcb41d271b9f394f8706b86c76e92061a7d
mac <toshi.import@gmail.com>
2018-10-17 02:52:47 +0900

    Articleモデルのバリデーションを設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,25 @@
 class Article < ActiveRecord::Base
+  # validation
+  # 見出し
+  validates :title, presence: true, length:{maximum: 32}
+  validates :image, presence: true
+  validates :details, presence: true
+  validates :published_at, presence: true, if: :published?
+
   # image_uploader(cloudinary)
   # attr_accessor :image
   mount_uploader :image, ArticleimageUploader
+
+  # validation condition
+  def published?
+    self.published == true
+  end
+
   def check_published_at
     self.published == true ? self.published_at = Time.now : self.published_at = ""
+  end
+
+  def count_pv
+    REDIS.zscore("articles", self.id).floor
   end
 end
