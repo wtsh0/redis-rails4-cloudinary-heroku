5e688e9ce8bdc275429c4f9d4ecd7612efdfbce8
mac <toshi.import@gmail.com>
2018-10-16 23:32:07 +0900

    ユーザー側のデザイン終了(仮)

--- app/helpers/public/articles_helper.rb
+++ app/helpers/public/articles_helper.rb
@@ -1,2 +1,9 @@
 module Public::ArticlesHelper
+  def dispImage(image)
+    if image.present?
+      image_tag image.url, width: 100
+    else
+      image_tag('/assets/no-image.png', width: 100, height: 100)
+    end
+  end
 end
