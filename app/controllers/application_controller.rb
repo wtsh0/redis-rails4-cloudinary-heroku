92bc6e5d0ccfc645e423df9f03fc1e52ce1cb6ba
mac <toshi.import@gmail.com>
2018-10-19 21:00:47 +0900

    deviseのログアウト時の向き先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
+
+  def after_sign_out_path_for(resource)
+    new_manager_session_path
+  end
 end
